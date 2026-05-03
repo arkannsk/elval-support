f42ec773356369b8242d449a586dfe35ce0e1579
A.Kashin <a.kashin@bftcom.com>
2026-05-03 23:55:04 +0700

    fix: hightlighter

--- src/main/kotlin/com/arkannsk/elval/highlighting/ElvalColorSettingsPage.kt
+++ src/main/kotlin/com/arkannsk/elval/highlighting/ElvalColorSettingsPage.kt
@@ -15,7 +15,37 @@ import javax.swing.Icon
 
 class ElvalColorSettingsPage : ColorSettingsPage {
 
-    override fun getDisplayName(): String = "ElVal Annotations"
+    companion object {
+        // Ключи атрибутов
+        val ELVAL_KEYWORD = TextAttributesKey.createTextAttributesKey(
+            "ELVAL_KEYWORD",
+            DefaultLanguageHighlighterColors.KEYWORD
+        )
+
+        val ELVAL_PARAM_NAME = TextAttributesKey.createTextAttributesKey(
+            "ELVAL_PARAM_NAME",
+            DefaultLanguageHighlighterColors.FUNCTION_CALL
+        )
+
+        val ELVAL_PARAM_VALUE = TextAttributesKey.createTextAttributesKey(
+            "ELVAL_PARAM_VALUE",
+            DefaultLanguageHighlighterColors.STRING
+        )
+
+        val OA_ANNOTATION = TextAttributesKey.createTextAttributesKey(
+            "OA_ANNOTATION",
+            DefaultLanguageHighlighterColors.METADATA
+        )
+
+        private val DESCRIPTORS = arrayOf(
+            AttributesDescriptor("ElVal - Directive (@evl:validate, @evl:decor)", ELVAL_KEYWORD),
+            AttributesDescriptor("ElVal - Parameter Name (min:, max:, pattern:, contains:)", ELVAL_PARAM_NAME),
+            AttributesDescriptor("ElVal - Parameter Value (required, email, 10, README)", ELVAL_PARAM_VALUE),
+            AttributesDescriptor("OpenAPI - Annotation (@oa:title, @oa:in, @oa:description)", OA_ANNOTATION),
+        )
+    }
+
+    override fun getDisplayName(): String = "ElVal & OpenAPI Annotations"
 
     override fun getIcon(): Icon? = null
 
@@ -42,18 +72,22 @@ class ElvalColorSettingsPage : ColorSettingsPage {
 
     @NotNull
     override fun getDemoText(): String {
-        // Используем теги <KEYWORD>, <PARAM_NAME> и т.д., которые мы замапим ниже
         return """
-            type User struct {
-                // <KEYWORD>@evl:validate</KEYWORD> <PARAM_NAME>min</PARAM_NAME>:<PARAM_VALUE>3</PARAM_VALUE> <PARAM_NAME>max</PARAM_NAME>:<PARAM_VALUE>50</PARAM_VALUE>
+            type Document struct {
+                // <KEYWORD>@evl:validate</KEYWORD> <PARAM_NAME>required</PARAM_NAME>
+                // <KEYWORD>@evl:validate</KEYWORD> <PARAM_NAME>contains:</PARAM_NAME><PARAM_VALUE>README</PARAM_VALUE>
                 Name string
                 
-                // <KEYWORD>@evl:validate</KEYWORD> <PARAM_NAME>pattern</PARAM_NAME>:<PARAM_VALUE>email</PARAM_VALUE>
-                // <OA>@oa:title</OA> <PARAM_VALUE>"User Email"</PARAM_VALUE>
-                Email string
+                // <KEYWORD>@evl:validate</KEYWORD> <PARAM_NAME>starts_with:</PARAM_NAME><PARAM_VALUE>https://</PARAM_VALUE>
+                // <KEYWORD>@evl:validate</KEYWORD> <PARAM_NAME>ends_with:</PARAM_NAME><PARAM_VALUE>.com</PARAM_VALUE>
+                URL string
                 
                 // <KEYWORD>@evl:decor</KEYWORD> <PARAM_VALUE>uuid-gen</PARAM_VALUE>
                 ID string
+                
+                // <OA>@oa:in</OA> <PARAM_VALUE>query</PARAM_VALUE> page
+                // <OA>@oa:description</OA> <PARAM_VALUE>"Page number"</PARAM_VALUE>
+                Page int
             }
         """.trimIndent()
     }
@@ -67,31 +101,3 @@ class ElvalColorSettingsPage : ColorSettingsPage {
         )
     }
 }
-
-// Создаем свои ключи атрибутов на основе стандартных цветов
-val ELVAL_KEYWORD = TextAttributesKey.createTextAttributesKey(
-    "ELVAL_KEYWORD",
-    DefaultLanguageHighlighterColors.KEYWORD
-)
-
-val ELVAL_PARAM_NAME = TextAttributesKey.createTextAttributesKey(
-    "ELVAL_PARAM_NAME",
-    DefaultLanguageHighlighterColors.NUMBER
-)
-
-val ELVAL_PARAM_VALUE = TextAttributesKey.createTextAttributesKey(
-    "ELVAL_PARAM_VALUE",
-    DefaultLanguageHighlighterColors.STRING
-)
-
-val OA_ANNOTATION = TextAttributesKey.createTextAttributesKey(
-    "OA_ANNOTATION",
-    DefaultLanguageHighlighterColors.METADATA
-)
-
-private val DESCRIPTORS = arrayOf(
-    AttributesDescriptor("ElVal - Keyword (@evl:validate, @evl:decor)", ELVAL_KEYWORD),
-    AttributesDescriptor("ElVal - Parameter Name (min, max, pattern)", ELVAL_PARAM_NAME),
-    AttributesDescriptor("ElVal - Parameter Value (required, 10, email)", ELVAL_PARAM_VALUE),
-    AttributesDescriptor("OpenAPI Annotation (@oa:title, @oa:description)", OA_ANNOTATION),
-)
